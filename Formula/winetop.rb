3120e7900e9e61cbf2e8a5e26dbd70564765c48a
winetop-ci <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-22 07:54:33 +0000

    winetop 0.1.5

--- Formula/winetop.rb
+++ Formula/winetop.rb
@@ -1,17 +1,17 @@
 class Winetop < Formula
   desc "htop for Wine prefixes"
   homepage "https://github.com/akovari/winetop"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   on_linux do
     on_intel do
       url "https://github.com/akovari/winetop/releases/download/v#{version}/winetop-v#{version}-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "cf04ad82853addf4e019c19edfb2795058dcc46add8553955ff534a7a434f9cd"
+      sha256 "0d939091f64ca10110465dd96a4d529d636bb054e59d1651d6ca2aca7f406f12"
     end
     on_arm do
       url "https://github.com/akovari/winetop/releases/download/v#{version}/winetop-v#{version}-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "f77c374fb0b4b174ac65c4c3b300366b06cde489d458e95d49fa99a360d3e3b2"
+      sha256 "5fd54d109ae908c0d5fad6cf1288a91c5bf306f7427f51ad53df3f255d44cc09"
     end
   end
 
